26c01f73168679c635a7b6d992c3f0d80c1a0a91
fouadhass <158025240+fouadhass@users.noreply.github.com>
2024-02-19 20:22:52 +0100

    Update ex6.sh

--- Act4/ex6.sh
+++ Act4/ex6.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
-# Script per comprovar l'estat dels servidors remots i registrar els resultats
 
 # Funció per comprovar l'estat d'un servidor remot
 comprovar_estat_servidor() {
+    # Assigna el primer argument de la funció (el nom del servidor) a la variable "servidor"
     servidor=$1
+    # Utilitza la comanda ping per comprovar si el servidor és accessible
+    # Si ping té èxit, l'estat és "ACTIU", si no, l'estat és "INACTIU"
     estat=$(ping -c 1 $servidor &> /dev/null && echo "ACTIU" || echo "INACTIU")
+    # Imprimeix la data, l'hora, el nom del servidor i l'estat en un fitxer de registre
     echo "$(date '+%Y-%m-%d %H:%M:%S') - $servidor: $estat" >> registre.txt
 }
 
 # Funció per comprovar l'estat de tots els servidors
 comprovar_tots_servidors() {
+    # Imprimeix un missatge indicant que comença la comprovació dels servidors
     echo "Comprovant l'estat dels servidors..."
+    # Itera sobre cada línia del fitxer "servidors.txt"
     while read -r servidor; do
+        # Crida a la funció "comprovar_estat_servidor" amb el nom del servidor com a argument
         comprovar_estat_servidor $servidor
+    # Finalitza el bucle quan s'han llegit totes les línies del fitxer
     done < servidors.txt
 }
 
-# Inici del script
+# Inicia el script
 echo "Inici de comprovació dels servidors."
+# Crida a la funció "comprovar_tots_servidors" per iniciar la comprovació dels servidors
 comprovar_tots_servidors
+# Imprimeix un missatge indicant que la comprovació ha acabat i que els resultats s'han registrat
 echo "Comprovació finalitzada. Els resultats s'han registrat a registre.txt."
